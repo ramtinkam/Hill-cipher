e5c6896a1b1854a4f997c7e9111416a3fe9b6693
Ramtin <ramtinkamanian82@gmail.com>
2024-06-26 22:12:04 +0330

    created the UI

--- Hill cipher.py
+++ Hill cipher.py
@@ -1,9 +1,41 @@
-from tkinter import *
+from tkinter import *   #char code starts at 0 ends at 93(ascii-33)
 
 root = Tk()
 
 root.title("Hill cipher")
 
-root.geometry('600x600')
+root.geometry('300x300')
+
+key = StringVar()
+text = StringVar()
+output = StringVar()
+
+keyLabel = Label(root, text="Key:")
+keyEntry = Entry(root,textvariable=key)
+
+textLabel = Label(root, text="Enter Text:")
+textEntry = Entry(root,textvariable=text)
+
+encodeButton = Button(root,text="Encode",command=encode)
+decodeButton = Button(root,text="Decode",command=decode)
+
+outputEntry = Entry(root,textvariable=output,state=DISABLED)
+
+keyLabel.pack(padx=1, pady=1)
+keyEntry.pack()
+
+textLabel.pack(padx=1, pady=1)
+textEntry.pack()
+
+encodeButton.pack(pady=10)
+decodeButton.pack()
+
+outputEntry.pack(pady=10)
+
+
+
+
+
+
 
 root.mainloop()
